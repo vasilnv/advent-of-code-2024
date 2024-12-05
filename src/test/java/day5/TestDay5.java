71aeb4cec190f1a2dc36a5457b7811ef1c18b031
vasilnv <vasil.n.v.1999@gmail.com>
2024-12-05 15:23:34 +0200

    Day 5. Add test for the solution of the second task

--- src/test/java/day5/TestDay5.java
+++ src/test/java/day5/TestDay5.java
@@ -1,13 +1,20 @@
 package day5;
 
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TestDay5 extends TestCase {
-    public void testTask1() {
-        String input = "47|53\n" +
+    String input;
+    List<List<Integer>> pairs;
+    List<List<Integer>> sequences;
+
+    @Before
+    public void setUp() throws Exception {
+        input = "47|53\n" +
                 "97|13\n" +
                 "97|61\n" +
                 "97|47\n" +
@@ -36,8 +43,13 @@ public class TestDay5 extends TestCase {
                 "61,13,29\n" +
                 "97,13,75,29,47";
 
-        List<List<Integer>> pairs = Task1.getPairs(input);
-        List<List<Integer>> sequences = Task1.extractSequences(input);
+        pairs = Task1.getPairs(input);
+        sequences = Task1.extractSequences(input);
+
+    }
+
+    @Test
+    public void testTask1() {
 
         Task1 task1 = new Task1();
         task1.createGraph(pairs);
@@ -50,6 +62,11 @@ public class TestDay5 extends TestCase {
         assertFalse(task1.checkSequence(sequences.get(5)));
 
         assertEquals(143, Task1.getTotalScore(sequences, pairs));
+    }
+
+    @Test
+    public void testTask2() {
+        assertEquals(123, Task1.getTotalScoreErroneous(sequences, pairs));
     }
 
 }
