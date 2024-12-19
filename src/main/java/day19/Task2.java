f9a4d35cd9de3472fb6b6b9b373faffac9918555
vasilnv <vasil.n.v.1999@gmail.com>
2024-12-19 18:05:28 +0200

    Day 19. Add Task2 faster solution

--- src/main/java/day19/Task2.java
+++ src/main/java/day19/Task2.java
@@ -4,44 +4,35 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Task2 extends Task1 {
-    Map<String, Set<List<String>>> combinationSuccess = new HashMap<>();
+    Map<String, Long> combinationSuccess = new HashMap<>();
 
-    public int solve() {
-        int result = 0;
+    public long solve2() {
+        long result = 0;
         for (String combination : combinations) {
-            System.out.println("CHECKING COMBINATION " + combination);
-            result += checkCombo(combination).size();
+            result += checkCombo(combination);
         }
         return result;
     }
 
-    private Set<List<String>> checkCombo(String combination) {
+    private long checkCombo(String combination) {
+        if (combination.isEmpty()) {
+            return 1;
+        }
         if (combinationSuccess.containsKey(combination)) {
             return combinationSuccess.get(combination);
         }
-        Set<List<String>> allCombos = new HashSet<>();
-        if (allTowels.contains(combination)) {
-            allCombos.add(List.of(combination));
-            return allCombos;
-        }
-        for (int i = 1; i < combination.length(); i++) {
-            Set<List<String>> left;
-            Set<List<String>> right;
-            left = checkCombo(combination.substring(0, i));
-            right = checkCombo(combination.substring(i));
-            for (List<String> combinationsLeft : left) {
-                for (List<String> combinationsRight : right) {
-                    List<String> resultList = new ArrayList<>();
-                    resultList.addAll(combinationsLeft);
-                    resultList.addAll(combinationsRight);
-                    allCombos.add(resultList);
-                }
+        long result = 0;
+        for (var combo : allTowels) {
+            if (combination.startsWith(combo)) {
+                result += checkCombo(combination.substring(combo.length()));
             }
         }
-        combinationSuccess.put(combination, allCombos);
-        return allCombos;
+        combinationSuccess.put(combination, result);
+        return result;
     }
 
     public static void main(String[] args) {
@@ -49,7 +40,7 @@ public class Task2 extends Task1 {
             String input = Files.readString(Path.of("src/main/resources/day19.txt"));
             Task2 task2 = new Task2();
             task2.convertInput(input);
-            System.out.println("Result is: " + task2.solve());
+            System.out.println("Result is: " + task2.solve2());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
